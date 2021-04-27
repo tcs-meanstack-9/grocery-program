20096f43dc30919bf70eb3f59ec23dc101339cdb
mohitha20 <mohitha.narayan1995@gmail.com>
2021-04-27 13:45:01 -0500

    home page created

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -12,10 +12,16 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MaterialModule } from './material/material.module';
 import { ProductDialogComponent } from './product-dialog/product-dialog.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  {path:'',component:HomeComponent},
+  { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  
+
 ];
 
 @NgModule({
@@ -24,7 +30,10 @@ const routes: Routes = [
     DashboardComponent,
     HeaderComponent,
     ProductDialogComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
